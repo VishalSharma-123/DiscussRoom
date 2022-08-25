3dd7eff069d0a834a1a785f69be53ebc855b39b6
Vishal Sharma <vishalsharma@Vishals-MacBook-Air.local>
2022-08-25 10:27:26 +0530

    Added the database for Rooms, Messages, and Topic for Discuss Room. Also pushed them in the migrations.

--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
-room = [
-    {'id': 1, 'name':'lets learn Python'},
-    {'id': 2, 'name':'lets learn C++'},
-    {'id': 3, 'name':'lets learn Java'},
-]
+# room = [
+#     {'id': 1, 'name':'lets learn Python'},
+#     {'id': 2, 'name':'lets learn C++'},
+#     {'id': 3, 'name':'lets learn Java'},
+# ]
 
 #should be names as tempaltes/base
 
 # triggerign rooms and home page
 def home(request):
+    room = Room.objects.all()
     context = {'rooms': room}
     return render(request, 'base/home.html', context)             #rendering the html file fromt template
 
 def rooms(request, pk):
-    rooms = None
-    for i in room:
-        if(i['id'] == int(pk)):
-            room_s = i
+    room_s = Room.objects.get(id = pk)
     context = {'room': room_s}
     return render(request, 'base/room.html',context)
